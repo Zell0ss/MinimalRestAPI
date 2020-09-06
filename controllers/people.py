9d8177217a4dd3d09667be636e2821800b5fe289
Josem Martin <zelloss@gmail.com>
2020-09-06 19:20:48 +0200

    notes functionality implemented

--- controllers/people.py
+++ controllers/people.py
@@ -1,8 +1,7 @@
 from flask import make_response, abort
 from config import db
 from models.person import  Person,  PersonSchema
-
-
+from models.note import  Note
 
 # Create a handler for our read (GET) people
 def read_all():
@@ -16,9 +15,10 @@ def read_all():
     # query the person database table for all the records (no filter),
     # sort them in ascending order (the default sorting order)
     # Create the list of person objects (people)
-    people = Person.query \
-        .order_by(Person.lname) \
-        .all()
+    people = (Person.query
+            .order_by(Person.lname)
+            .all()
+    )
 
     # Serialize the data for the response (marshmallow)
     # many=true => expect an iterable rather than only one record
@@ -30,27 +30,28 @@ def read_one(person_id):
     """
     GET /api/people/{person_id}
     This function responds to a request for /api/people/{person_id}
-    with one matching person from people
+    with one matching person from people and his posts
 
     :param person_id:   id of person to find
     :return:        person matching last name
     """
     # Get the person requested: query for one or none with the ID passed
     # If none found returns None instead an empty Person
-    person = Person.query \
-        .filter(Person.person_id == person_id) \
+    person = (Person.query
+        .filter(Person.person_id == person_id)
+        .outerjoin(Note)
         .one_or_none()
+    )
 
     # Does the person exist in people? if yes: serialize (one object so many=False)
     # and return
     if person is not None:
         person_schema = PersonSchema()
-        return person_schema.dump(person).data
+        data = person_schema.dump(person)
+        return data
 
     # otherwise, nope, not found
-    abort(
-        404, f"Person with ID: {person_id} not found in database"
-    )
+    abort(404, f"Person with ID: {person_id} not found in database")
 
 
 
@@ -69,11 +70,12 @@ def create(person):
     fname = person.get("fname", None)
 
     # Does the person exist already?
-    existing_person  = Person.query \
-        .filter(Person.heroname == heroname) \
-        .filter(Person.fname == fname) \
-        .filter(Person.lname == lname) \
+    existing_person  = (Person.query
+        .filter(Person.heroname == heroname)
+        .filter(Person.fname == fname)
+        .filter(Person.lname == lname)
         .one_or_none()
+        )
 
     if existing_person is None:
         # Create a person instance using the schema and the passed-in person
@@ -153,7 +155,7 @@ def update(person_id, person):
 
 def delete(person_id):
     """
-    This function deletes a person from the people structure
+    This function deletes a person and his psots from the people structure
 
     :param person_id:   person_id name of person to delete
     :return:        200 on successful delete, 404 if not found
